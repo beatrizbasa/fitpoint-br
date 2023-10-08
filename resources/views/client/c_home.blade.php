38a1f8685b0a64ad7b051c1cf5901865bfa1b0a8
beatriz basa <beatrizbasa918@gmail.com>
2023-10-08 22:09:53 +0800

    from r

--- resources/views/client/c_home.blade.php
+++ resources/views/client/c_home.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <!-- rochelle -->
+<head> 
     <title>A&T Fitness Center</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
